4573b7e276221c8c7e0d693718482e05107b16e3
popcornmix <popcornmix@gmail.com>
2012-10-05 17:43:41 +0100

    Move to version 5 of VCHIQ. Note: this requires a corresponding start.elf and /opt/vc/lib update

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_build_info.h
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_build_info.h
@@ -16,38 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/*=============================================================================
-VideoCore OS Abstraction Layer - 'once'
-=============================================================================*/
-
-#ifndef VCOS_ONCE_H
-#define VCOS_ONCE_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "interface/vcos/vcos_types.h"
-#include "vcos_platform.h"
-
-/**
- * \file vcos_once.h
- *
- * Ensure something is called only once.
- *
- * Initialize once_control to VCOS_ONCE_INIT. The first
- * time this is called, the init_routine will be called. Thereafter
- * it won't.
- *
- * \sa pthread_once()
- *
- */
-
-VCOS_STATUS_T vcos_once(VCOS_ONCE_T *once_control,
-                        void (*init_routine)(void));
-
-#ifdef __cplusplus
-}
-#endif
-#endif
-
+const char *vchiq_get_build_hostname(void);
+const char *vchiq_get_build_version(void);
+const char *vchiq_get_build_time(void);
+const char *vchiq_get_build_date(void);
